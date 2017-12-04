839a4bf48312ab47400fb10cf0b3f0a60320103d
IACJ <iacjacj@qq.com>
2017-12-04 18:31:02 +0800

    期末考试前一天是最强的

---
+++ workspace/review_简单编程示例/src/com/acj/rmi/HelloInterface.java
@@ -0,0 +1,9 @@
+package com.acj.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface HelloInterface extends Remote {
+
+	public String sayHello(String name) throws RemoteException;
+}
